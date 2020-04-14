14111f1a8dbba73590bc40ea271be2f1d48c13b1
D19A0958 educacion <D19A0958@educacion.edu.do>
2020-04-14 05:54:01 -0400

    Agregar archivos de proyecto.

---
+++ Pruebas/CheckConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Pruebas
+{
+    public class CheckConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? resultado = null;
+            bool algunTrue = false, algunFalse = false;
+
+            foreach (bool isCheked in values){
+                if (isCheked){
+                    algunTrue = true;
+                }
+                else
+                {
+                    algunFalse = true;
+                }
+            }
+            if(algunTrue && !algunFalse)
+            {
+                resultado = true;
+            }else if(algunFalse && !algunTrue)
+            {
+                resultado = false;
+            }
+            return resultado;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            object[] estados = new object[] { value, value, value, value, value };
+            return estados;
+        }
+    }
+}
